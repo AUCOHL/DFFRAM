24745c4b2a0bf9264bca36969d771187040c8b11
nofal <ahmednofal@aucegypt.edu>
2021-04-13 20:23:44 +0200

    resetting the Handcrafter/Verification folder to the original state

---
+++ Compiler/verification/gen_tb.py
@@ -0,0 +1,14 @@
+import tb_template
+import sys
+import re
+import math
+
+if __name__ == "__main__":
+    PATTERN = sys.argv[1]
+    word_num = int(re.search('tb_RAM(.*)x32', PATTERN).group(1))
+    address_lines_num = int(math.log2(word_num))
+    tb = tb_template.changeable_sub.format(word_num,
+            address_lines_num)
+    tb += tb_template.constant_sub
+    with open('tb_RAM{}x32.v'.format(word_num), 'w+') as f:
+        f.write(tb)
