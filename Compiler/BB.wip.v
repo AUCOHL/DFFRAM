cbe0072384b97df42610e6c836cef9719ed6ce11
nofal <ahmednofal@aucegypt.edu>
2021-04-26 08:08:55 +0200

    testbenches for dual ported RAM, RAM32x64_1RW1R , RAM32x32_1RW1R passing

--- Compiler/BB.wip.v
+++ Compiler/BB.wip.v
@@ -1,3 +1,36 @@
+module DEC2x4 (
+    input           EN,
+    input   [1:0]   A,
+    output  [3:0]   SEL
+);
+    sky130_fd_sc_hd__nor3b_4    AND0 ( .Y(SEL[0]), .A(A[0]),   .B(A[1]), .C_N(EN) );
+    sky130_fd_sc_hd__and3b_4    AND1 ( .X(SEL[1]), .A_N(A[1]), .B(A[0]), .C(EN) );
+    sky130_fd_sc_hd__and3b_4    AND2 ( .X(SEL[2]), .A_N(A[0]), .B(A[1]), .C(EN) );
+    sky130_fd_sc_hd__and3_4     AND3 ( .X(SEL[3]), .A(A[1]),   .B(A[0]), .C(EN) );
+    
+endmodule
+
+module DEC3x8 (
+    input           EN,
+    input [2:0]     A,
+    output [7:0]    SEL
+);
+
+    wire [2:0]  A_buf;
+    wire        EN_buf;
+
+    sky130_fd_sc_hd__clkbuf_2 ABUF[2:0] (.X(A_buf), .A(A));
+    sky130_fd_sc_hd__clkbuf_2 ENBUF (.X(EN_buf), .A(EN));
+    
+    sky130_fd_sc_hd__nor4b_2   AND0 ( .Y(SEL[0])  , .A(A_buf[0]), .B(A_buf[1])  , .C(A_buf[2]), .D_N(EN_buf) ); // 000
+    sky130_fd_sc_hd__and4bb_2   AND1 ( .X(SEL[1])  , .A_N(A_buf[2]), .B_N(A_buf[1]), .C(A_buf[0])  , .D(EN_buf) ); // 001
+    sky130_fd_sc_hd__and4bb_2   AND2 ( .X(SEL[2])  , .A_N(A_buf[2]), .B_N(A_buf[0]), .C(A_buf[1])  , .D(EN_buf) ); // 010
+    sky130_fd_sc_hd__and4b_2    AND3 ( .X(SEL[3])  , .A_N(A_buf[2]), .B(A_buf[1]), .C(A_buf[0])  , .D(EN_buf) );   // 011
+    sky130_fd_sc_hd__and4bb_2   AND4 ( .X(SEL[4])  , .A_N(A_buf[0]), .B_N(A_buf[1]), .C(A_buf[2])  , .D(EN_buf) ); // 100
+    sky130_fd_sc_hd__and4b_2    AND5 ( .X(SEL[5])  , .A_N(A_buf[1]), .B(A_buf[0]), .C(A_buf[2])  , .D(EN_buf) );   // 101
+    sky130_fd_sc_hd__and4b_2    AND6 ( .X(SEL[6])  , .A_N(A_buf[0]), .B(A_buf[1]), .C(A_buf[2])  , .D(EN_buf) );   // 110
+    sky130_fd_sc_hd__and4_2     AND7 ( .X(SEL[7])  , .A(A_buf[0]), .B(A_buf[1]), .C(A_buf[2])  , .D(EN_buf) ); // 111
+endmodule
 module BYTE #(  parameter   USE_LATCH=1)( 
     input   wire        CLK,    // FO: 1
     input   wire        WE,     // FO: 1
@@ -93,7 +126,7 @@ module WORD #( parameter    USE_LATCH=1,
     generate
         genvar i;
             for(i=0; i<SIZE; i=i+1) 
-                BYTE #(.USE_LATCH(USE_LATCH)) B ( .CLK(CLK_buf), .WE(WE[i]), .SEL(SEL_buf), .Di(Di[i*8-1:0]), .Do(Do[i*8-1:0]) );
+                BYTE #(.USE_LATCH(USE_LATCH)) B ( .CLK(CLK_buf), .WE(WE[i]), .SEL(SEL_buf), .Di(Di[(i+1)*8-1:i*8]), .Do(Do[(i+1)*8-1:i*8]) );
     endgenerate
     
 endmodule 
@@ -122,15 +155,15 @@ module WORD_1RW1R #( parameter  USE_LATCH=1,
                     .WE(WE[i]), 
                     .SEL0(SEL0_buf), 
                     .SEL1(SEL1_buf), 
-                    .Di(Di[i*8-1:0]), 
-                    .Do0(Do0[i*8-1:0]),
-                    .Do1(Do1[i*8-1:0])  
+                    .Di(Di[(i+1)*8-1:i*8]), 
+                    .Do0(Do0[(i+1)*8-1:i*8]),
+                    .Do1(Do1[(i+1)*8-1:i*8])  
                 );
     endgenerate
     
 endmodule 
 
-module SLICE #( parameter    USE_LATCH=1,
+module RAM8 #( parameter    USE_LATCH=1,
                             SIZE=1 ) (
     input   wire                CLK,    // FO: 1
     input   wire [SIZE-1:0]     WE,     // FO: 1
@@ -140,9 +173,9 @@ module SLICE #( parameter    USE_LATCH=1,
     output  wire [(SIZE*8-1):0] Do
 );
 
-    wire    [7:0]   SEL;
-    wire            WE_buf; 
-    wire            CLK_buf;
+    wire    [7:0]        SEL;
+    wire    [SIZE-1:0]   WE_buf; 
+    wire                 CLK_buf;
 
     DEC3x8 DEC (.EN(EN), .A(A), .SEL(SEL));
     sky130_fd_sc_hd__clkbuf_2 WEBUF (.X(WE_buf), .A(WE));
@@ -157,7 +190,7 @@ module SLICE #( parameter    USE_LATCH=1,
 
 endmodule
 
-module SLICE_1RW1R #( parameter     USE_LATCH=1,
+module RAM8_1RW1R #( parameter     USE_LATCH=1,
                                     SIZE=1 ) (
     input   wire                CLK,    // FO: 1
     input   wire [SIZE-1:0]     WE,     // FO: 1
@@ -170,14 +203,14 @@ module SLICE_1RW1R #( parameter     USE_LATCH=1,
     output  wire [(SIZE*8-1):0] Do1
 );
 
-    wire    [7:0]   SEL0, SEL1;
-    wire            WE_buf; 
-    wire            CLK_buf;
+    wire    [7:0]          SEL0, SEL1;
+    wire    [SIZE-1:0]     WE_buf; 
+    wire                   CLK_buf;
 
     DEC3x8 DEC0 (.EN(EN0), .A(A0), .SEL(SEL0));
     DEC3x8 DEC1 (.EN(EN1), .A(A1), .SEL(SEL1));
     
-    sky130_fd_sc_hd__clkbuf_2 WEBUF (.X(WE_buf), .A(WE));
+    sky130_fd_sc_hd__clkbuf_2 WEBUF[(SIZE-1):0]   (.X(WE_buf), .A(WE));
     sky130_fd_sc_hd__clkbuf_2 CLKBUF (.X(CLK_buf), .A(CLK));
 
     generate
@@ -197,8 +230,8 @@ module SLICE_1RW1R #( parameter     USE_LATCH=1,
 
 endmodule
 
-// 8 Slices block (128 bytes) with registered outout 
-module BLOCK #( parameter   USE_LATCH=1,
+// 4 Slices block (128 bytes) with registered outout 
+module RAM32 #( parameter   USE_LATCH=1,
                             SIZE=1 ) (
     input   wire                CLK,    // FO: 1
     input   wire [SIZE-1:0]     WE,     // FO: 1
@@ -211,7 +244,7 @@ module BLOCK #( parameter   USE_LATCH=1,
     wire [3:0]          SEL;
     wire [4:0]          A_buf;
     wire                CLK_buf;
-    wire [SIZe-1:0]     WE_buf;
+    wire [SIZE-1:0]     WE_buf;
     wire                EN_buf;
 
     wire [(SIZE*8-1):0] Do_pre;
@@ -231,7 +264,7 @@ module BLOCK #( parameter   USE_LATCH=1,
     generate
         genvar i;
         for (i=0; i< 4; i=i+1) begin : SLICE
-            SLICE #(.USE_LATCH(USE_LATCH), .SIZE(SIZE)) RAM8 (.CLK(CLK_buf), .WE(WE_buf),.EN(SEL[i]), .Di(Di_buf), .Do(Do_pre), .A(A_buf[2:0]) ); 
+            RAM8 #(.USE_LATCH(USE_LATCH), .SIZE(SIZE)) RAM8 (.CLK(CLK_buf), .WE(WE_buf),.EN(SEL[i]), .Di(Di_buf), .Do(Do_pre), .A(A_buf[2:0]) ); 
         end
     endgenerate
 
@@ -239,21 +272,21 @@ module BLOCK #( parameter   USE_LATCH=1,
     wire [SIZE-1:0] lo;
     wire [SIZE-1:0] float_buf_en;
     sky130_fd_sc_hd__clkbuf_2   FBUFENBUF[SIZE-1:0] ( .X(float_buf_en), .A(EN) );
-    sky130_fd_sc_hd__conb_1     TIE[SIZ-1:0] (.LO(lo), .HI());
+    sky130_fd_sc_hd__conb_1     TIE[SIZE-1:0] (.LO(lo), .HI());
 
     // Following split by group because each is done by one TIE CELL and ONE CLKINV_4
     // Provides default values for floating lines (lo)
     generate
         for (i=0; i< SIZE; i=i+1) begin : BYTE
-            sky130_fd_sc_hd__ebufn_2 FLOATBUF_B0[i*SIZE-1:0] ( .A( lo[i] ), .Z(Do_pre[i*SIZE-1:0]), .TE_B(float_buf_en[i]) );        
+            sky130_fd_sc_hd__ebufn_2 FLOATBUF_B0[(8*(i+1))-1:8*i] ( .A( lo[i] ), .Z(Do_pre[(8*(i+1))-1:8*i]), .TE_B(float_buf_en[i]) );        
         end
     endgenerate
     
-    sky130_fd_sc_hd__dfxtp_1 Do_FF[SIZE*9-1:0] ( .D(Do_pre), .Q(Do), .CLK(CLK) );
+    sky130_fd_sc_hd__dfxtp_1 Do_FF[SIZE*8-1:0] ( .D(Do_pre), .Q(Do), .CLK(CLK) );
 
 endmodule
 
-module BLOCK_1RW1R #( parameter     USE_LATCH=1,
+module RAM32_1RW1R #( parameter     USE_LATCH=1,
                                     SIZE=1 ) (
     input   wire                CLK,    // FO: 1
     input   wire [SIZE-1:0]     WE,     // FO: 1
@@ -269,7 +302,7 @@ module BLOCK_1RW1R #( parameter     USE_LATCH=1,
     wire [3:0]          SEL0, SEL1;
     wire [4:0]          A0_buf, A1_buf;
     wire                CLK_buf;
-    wire [SIZe-1:0]     WE_buf;
+    wire [SIZE-1:0]     WE_buf;
     wire                EN0_buf, EN1_buf;
 
     wire [(SIZE*8-1):0] Do0_pre, Do1_pre;
@@ -281,19 +314,19 @@ module BLOCK_1RW1R #( parameter     USE_LATCH=1,
     // Control signals buffers
     sky130_fd_sc_hd__clkbuf_2   CLKBUF              (.X(CLK_buf), .A(CLK));
     sky130_fd_sc_hd__clkbuf_2   WEBUF[(SIZE-1):0]   (.X(WE_buf), .A(WE));
-    sky130_fd_sc_hd__clkbuf_2   A0BUF[4:0]           (.X(A0_buf), .A(A0[4:0]));
-    sky130_fd_sc_hd__clkbuf_2   A1BUF[4:0]           (.X(A1_buf), .A(A1[4:0]));
+    sky130_fd_sc_hd__clkbuf_2   A0BUF[4:0]           (.X(A0_buf), .A(A0));
+    sky130_fd_sc_hd__clkbuf_2   A1BUF[4:0]           (.X(A1_buf), .A(A1));
     sky130_fd_sc_hd__clkbuf_2   EN0BUF               (.X(EN0_buf), .A(EN0));
     sky130_fd_sc_hd__clkbuf_2   EN1BUF               (.X(EN1_buf), .A(EN1));
 
-    DEC2x4 DEC0 (.EN(EN_buf), .A(A0_buf[4:3]), .SEL(SEL0));
-    DEC2x4 DEC1 (.EN(EN_buf), .A(A1_buf[4:3]), .SEL(SEL1));
+    DEC2x4 DEC0 (.EN(EN0_buf), .A(A0_buf[4:3]), .SEL(SEL0));
+    DEC2x4 DEC1 (.EN(EN1_buf), .A(A1_buf[4:3]), .SEL(SEL1));
 
 
     generate
         genvar i;
         for (i=0; i< 4; i=i+1) begin : SLICE
-            SLICE_1RW1R #(.USE_LATCH(USE_LATCH), .SIZE(SIZE)) RAM8 (
+            RAM8_1RW1R #(.USE_LATCH(USE_LATCH), .SIZE(SIZE)) RAM8_1RW1R (
                 .CLK(CLK_buf), 
                 .WE(WE_buf),
                 .EN0(SEL0[i]), 
@@ -312,21 +345,21 @@ module BLOCK_1RW1R #( parameter     USE_LATCH=1,
     wire [SIZE-1:0] float_buf_en0, float_buf_en1;
     sky130_fd_sc_hd__clkbuf_2   FBUFENBUF0[SIZE-1:0] ( .X(float_buf_en0), .A(EN0) );
     sky130_fd_sc_hd__clkbuf_2   FBUFENBUF1[SIZE-1:0] ( .X(float_buf_en1), .A(EN1) );
-    sky130_fd_sc_hd__conb_1     TIE0[SIZ-1:0] (.LO(lo0), .HI());
-    sky130_fd_sc_hd__conb_1     TIE1[SIZ-1:0] (.LO(lo1), .HI());
+    sky130_fd_sc_hd__conb_1     TIE0[SIZE-1:0] (.LO(lo0), .HI());
+    sky130_fd_sc_hd__conb_1     TIE1[SIZE-1:0] (.LO(lo1), .HI());
     
 
     // Following split by group because each is done by one TIE CELL and ONE CLKINV_4
     // Provides default values for floating lines (lo)
     generate
         for (i=0; i< SIZE; i=i+1) begin : BYTE
-            sky130_fd_sc_hd__ebufn_2 FLOATBUF0[i*SIZE-1:0] ( .A( lo0[i] ), .Z(Do0_pre[i*SIZE-1:0]), .TE_B(float_buf_en0[i]) );
-            sky130_fd_sc_hd__ebufn_2 FLOATBUF1[i*SIZE-1:0] ( .A( lo1[i] ), .Z(Do1_pre[i*SIZE-1:0]), .TE_B(float_buf_en1[i]) );
+            sky130_fd_sc_hd__ebufn_2 FLOATBUF0[(8*(i+1))-1:8*i] ( .A( lo0[i] ), .Z(Do0_pre[(8*(i+1))-1:8*i]), .TE_B(float_buf_en0[i]) );
+            sky130_fd_sc_hd__ebufn_2 FLOATBUF1[(8*(i+1))-1:8*i] ( .A( lo1[i] ), .Z(Do1_pre[(8*(i+1))-1:8*i]), .TE_B(float_buf_en1[i]) );
         end
     endgenerate
     
-    sky130_fd_sc_hd__dfxtp_1 Do0_FF[SIZE*9-1:0] ( .D(Do0_pre), .Q(Do0), .CLK(CLK) );
-    sky130_fd_sc_hd__dfxtp_1 Do1_FF[SIZE*9-1:0] ( .D(Do1_pre), .Q(Do1), .CLK(CLK) );
+    sky130_fd_sc_hd__dfxtp_1 Do0_FF[SIZE*8-1:0] ( .D(Do0_pre), .Q(Do0), .CLK(CLK) );
+    sky130_fd_sc_hd__dfxtp_1 Do1_FF[SIZE*8-1:0] ( .D(Do1_pre), .Q(Do1), .CLK(CLK) );
     
 
 endmodule
--- Compiler/verification/tb_template_1RW1R.py
+++ Compiler/verification/tb_template_1RW1R.py
@@ -1,7 +1,8 @@
 changeable_sub = """
 /*
-    An auto generated testbench to verify RAM{0}x32
-    Author: Mohamed Shalan (mshalan@aucegypt.edu)
+    An auto generated testbench to verify RAM{word_num}x{word_size}
+    Authors:Mohamed Shalan (mshalan@aucegypt.edu)
+            Ahmed Nofal (nofal.o.ahmed@gmail.com)
 */
 `define     VERBOSE_1
 `define     VERBOSE_2
@@ -10,43 +11,50 @@ changeable_sub = """
 
 `define     USE_LATCH   1
 
+`define     SIZE        {word_size}/8
 //`include "libs.ref/sky130_fd_sc_hd/verilog/primitives.v"
 //`include "libs.ref/sky130_fd_sc_hd/verilog/sky130_fd_sc_hd.v"
 
 `include "hd_primitives.v"
 `include "hd_functional.v"
 
-`include "BB.v"
+`include "BB.wip.v"
+// `include "BB.v"
 
-module tb_RAM{0}x32_1RW1R;
+module tb_RAM{word_num}x{word_size}_1RW1R;
 
-    localparam A_W = {1}+2;
+    localparam SIZE = `SIZE;
+    localparam A_W = {addr_width}+2;
     localparam M_SZ = 2**A_W;
 
-    reg             CLK;
-    reg  [3:0]      WE;
-    reg             EN;
-    reg  [31:0]     Di;
-    wire [31:0]     Do0;
-    wire [31:0]     Do1;
-    reg  [A_W-1:0]  A1, A2, ADDR;
+    reg                   CLK;
+    reg  [(SIZE-1):0]       WE;
+    reg                  EN0;
+    reg                  EN1;
+    reg  [(SIZE*8-1):0]   Di;
+    wire [(SIZE*8-1):0]   Do0;
+    wire [(SIZE*8-1):0]   Do1;
+    reg  [A_W-1:0]  A0, A1, ADDR;
     reg  [3:0]      HEX_DIG;
     reg  [7:0]      Phase;
-
+    reg  [7:0]      RANDOM_BYTE;
     event           done;
 
-    RAM{0}x32 #(.USE_LATCH(`USE_LATCH)) SRAM (
+    RAM{word_num}_1RW1R #(.USE_LATCH(`USE_LATCH), .SIZE(`SIZE)) SRAM (
         .CLK(CLK),
         .WE(WE),
-        .EN(EN),
+        .EN0(EN0),
+        .EN1(EN1),
         .Di(Di),
-        .Do(Do),
-        .A(A[A_W-1:2])
+        .Do0(Do0),
+        .Do1(Do1),
+        .A0(A0[A_W-1:2]),
+        .A1(A1[A_W-1:2])
     );
 
     initial begin
-        $dumpfile("tb_RAM{0}x32.vcd");
-        $dumpvars(0, tb_RAM{0}x32);
+        $dumpfile("tb_RAM{word_num}x{word_size}_1RW1R.vcd");
+        $dumpvars(0, tb_RAM{word_num}x{word_size}_1RW1R);
         @(done) $finish;
     end
 
@@ -57,33 +65,49 @@ constant_sub="""
     integer i;
 
      /* Memory golden Model */
-    reg [31:0]      RAM[(M_SZ)-1 : 0];
-    reg [31:0]      RAM_DATA;
+    reg [(SIZE*8-1):0]      RAM[(M_SZ)-1 : 0];
+    reg [(SIZE*8-1):0]      RAM_DATA0;
+    reg [(SIZE*8-1):0]      RAM_DATA1;
 
-    always @(posedge CLK)
-        if(EN) begin
-            RAM_DATA <= RAM[A/4];
-            if(WE[0]) RAM[A/4][ 7: 0] <= Di[7:0];
-            if(WE[1]) RAM[A/4][15:8] <= Di[15:8];
-            if(WE[2]) RAM[A/4][23:16] <= Di[23:16];
-            if(WE[3]) RAM[A/4][31:24] <= Di[31:24];
+    genvar c;
+    generate
+    for (c=0; c < SIZE; c = c+1) begin: mem_golden_model
+        always @(posedge CLK) begin
+            if(EN0) begin
+                RAM_DATA0 <= RAM[A0/4];
+                if(WE[c]) RAM[A0/4][8*(c+1)-1:8*c] <= Di[8*(c+1)-1:8*c];
+            end
+            if (EN1) begin
+                RAM_DATA1 <= RAM[A1/4];
+            end
         end
+    end
+    endgenerate
 
     initial begin
         CLK = 0;
         WE = 0;
-        EN = 1;
+        EN0 = 1;
+        EN1 = 1;
+
 
         Phase = 0;
-        // Perform a single word write then read
-        mem_write_word(32'hA5337090, 4);
-        mem_read_word(4);
+        // Perform a 2 word write then read 2 words
+        mem_write_word({SIZE{8'h90}}, 4);
+        mem_write_word({SIZE{8'h33}}, 8);
+        mem_read_2words(4,8);
+
+/***********************************************************
+
+        Write and read from different ports
+
+************************************************************/
 
         // Fill the memory with a known pattern
         for(i=0; i<M_SZ; i=i+4) begin
             HEX_DIG = $urandom%16;
             mem_write_word({8{HEX_DIG}},i);
-            mem_read_word(i);
+            mem_read_word_1(i);
         end
 
         // Word Write then Read
@@ -93,8 +117,9 @@ constant_sub="""
 `endif
         for(i=0; i<M_SZ; i=i+4) begin
             ADDR = (($urandom%M_SZ)) & 'hFFFF_FFFC ;
-            mem_write_word( $urandom, ADDR);
-            mem_read_word( ADDR );
+            RANDOM_BYTE = $urandom;
+            mem_write_word( {SIZE{RANDOM_BYTE}}, ADDR);
+            mem_read_word_1( ADDR );
         end
 
         // HWord Write then Read
@@ -105,7 +130,7 @@ constant_sub="""
         for(i=0; i<M_SZ; i=i+2) begin
             ADDR = (($urandom%M_SZ)) & 'hFFFF_FFFE;
             mem_write_hword($urandom&'hFFFF, ADDR );
-            mem_read_word( ADDR & 'hFFFF_FFFC );
+            mem_read_word_1( ADDR & 'hFFFF_FFFC );
         end
 
         // Byte Write then Read
@@ -116,7 +141,44 @@ constant_sub="""
         for(i=0; i<M_SZ; i=i+1) begin
             ADDR = (($urandom%M_SZ));
             mem_write_byte($urandom%255, ADDR);
-            mem_read_word(ADDR & 'hFFFF_FFFC );
+            mem_read_word_1(ADDR & 'hFFFF_FFFC );
+        end
+
+/***********************************************************
+
+        Write and read from same port
+
+************************************************************/
+        Phase = 4;
+`ifdef  VERBOSE_1
+        $display("\\nFinished Phase 3, starting Phase 4");
+`endif
+        for(i=0; i<M_SZ; i=i+4) begin
+            ADDR = (($urandom%M_SZ)) & 'hFFFF_FFFC ;
+            mem_write_word( $urandom, ADDR);
+            mem_read_word_0( ADDR );
+        end
+
+        // HWord Write then Read
+        Phase = 5;
+`ifdef  VERBOSE_1
+        $display("\\nFinished Phase 4, starting Phase 5");
+`endif
+        for(i=0; i<M_SZ; i=i+2) begin
+            ADDR = (($urandom%M_SZ)) & 'hFFFF_FFFE;
+            mem_write_hword($urandom&'hFFFF, ADDR );
+            mem_read_word_0( ADDR & 'hFFFF_FFFC );
+        end
+
+        // Byte Write then Read
+        Phase = 6;
+`ifdef  VERBOSE_1
+        $display("\\nFinished Phase 5, starting Phase 6");
+`endif
+        for(i=0; i<M_SZ; i=i+1) begin
+            ADDR = (($urandom%M_SZ));
+            mem_write_byte($urandom%255, ADDR);
+            mem_read_word_0(ADDR & 'hFFFF_FFFC );
         end
         $display ("\\n>> Test Passed! <<\\n");
         -> done;
@@ -125,7 +187,7 @@ constant_sub="""
     task mem_write_byte(input [7:0] byte, input [A_W-1:0] addr);
     begin
         @(posedge CLK);
-        A = addr;//[A_WIDTH:2];
+        A0 = addr;//[A_WIDTH:2];
         WE = (1 << addr[1:0]);
         Di = (byte << (addr[1:0] * 8));
         @(posedge CLK);
@@ -139,64 +201,90 @@ constant_sub="""
     task mem_write_hword(input [15:0] hword, input [A_W-1:0] addr);
     begin
         @(posedge CLK);
-        A = addr;//[A_WIDTH:2];
+        A0 = addr;//[A_WIDTH:2];
         WE = {{2{addr[1]}},{2{~addr[1]}}};
         Di = (hword << (addr[1] * 16));
         @(posedge CLK);
 `ifdef  VERBOSE_2
-        $display("WRITE BYTE: 0x%X to %0X(%0D) (0x%X, %B)", hword, addr, addr, Di, WE);
+        $display("WRITE HALFWORD: 0x%X to %0X(%0D) (0x%X, %B)", hword, addr, addr, Di, WE);
 `endif
         WE = 4'b0;
     end
     endtask
 
-    task mem_write_word(input [31:0] word, input [A_W-1:0] addr);
+    task mem_write_word(input [SIZE*8-1:0] word, input [A_W-1:0] addr);
     begin
         @(posedge CLK);
-        A = addr;
-        WE = 4'b1111;
+        A0 = addr;
+        WE = {SIZE{8'hFF}};
         Di = word;
         @(posedge CLK);
 `ifdef  VERBOSE_2
-        $display("WRITE BYTE: 0x%X to %0X(%0D) (0x%X, %B)", word, addr, addr, Di, WE);
+        $display("WRITE WORD: 0x%X to %0X(%0D) (0x%X, %B)", word, addr, addr, Di, WE);
 `endif
         WE = 4'b0;
     end
     endtask
 
-    task mem_read_word(input [A_W-1:0] addr);
+    task mem_read_word_0(input [A_W-1:0] addr);
     begin
         @(posedge CLK);
-        A = addr;//[9:2];
-        WE = 4'b0;
+        A0 = addr;//[9:2];
+        WE = {SIZE{8'h00}};
         @(posedge CLK);
         #5;
 `ifdef  VERBOSE_2
-        $display("READ WORD: 0x%X from %0D", Do, addr);
+        $display("READ WORD: 0x%X from %0D", Do0, addr);
 `endif
-        check();
-    end
-    endtask
-    task mem_read_2halfwords(input [A_W-1:0] addr1,
-                            input [A_W-1:0] addr2);
-    begin
-        @(posedge CLK);
-        A1 = addr1;//[9:2];
-        A2 = addr2;//[9:2];
-        WE = 4'b0;
-        @(posedge CLK);
-        #5;
-`ifdef  VERBOSE_2
-        $display("READ WORD: 0x%X from %0D", Do, addr);
-`endif
-        check();
+        check0();
     end
     endtask
 
-    task check; begin
-        if(RAM_DATA !== Do) begin
-            $display("\\n>>Test Failed! <<\\t(Pahse: %0d, Iteration: %0d", Phase, i);
-            $display("Address: 0x%X, READ: 0x%X - Should be: 0x%X", A, Do, RAM[A/4]);
+    task mem_read_word_1(input [A_W-1:0] addr);
+    begin
+        @(posedge CLK);
+        A1 = addr;//[9:2];
+        WE = {SIZE{8'h00}};
+        @(posedge CLK);
+        #5;
+`ifdef  VERBOSE_2
+        $display("READ WORD: 0x%X from %0D", Do1, addr);
+`endif
+        check1();
+    end
+    endtask
+
+    task mem_read_2words(input [A_W-1:0] addr0,
+            input [A_W-1:0] addr1);
+    begin
+        @(posedge CLK);
+        A0= addr0;//[9:2];
+        A1= addr1;//[9:2];
+        WE = 4'b0;
+        @(posedge CLK);
+        #5;
+`ifdef  VERBOSE_2
+        $display("READ WORD0: 0x%X from %0D", Do0, addr0);
+        $display("READ WORD1: 0x%X from %0D", Do1, addr1);
+`endif
+        check0();
+        check1();
+    end
+    endtask
+
+    task check0; begin
+        if(RAM_DATA0 !== Do0) begin
+            $display("\\n>>Test Failed! <<\\t(Phase: %0d, Iteration: %0d", Phase, i);
+            $display("Address: 0x%X, READ: 0x%X - Should be: 0x%X", A0, Do0, RAM[A0/4]);
+            $fatal(1);
+        end
+    end
+    endtask
+
+    task check1; begin
+        if(RAM_DATA1 !== Do1) begin
+            $display("\\n>>Test Failed! <<\\t(Phase: %0d, Iteration: %0d", Phase, i);
+            $display("Address: 0x%X, READ: 0x%X - Should be: 0x%X", A1, Do1, RAM[A1/4]);
             $fatal(1);
         end
     end
