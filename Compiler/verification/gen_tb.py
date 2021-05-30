f54b07407da55815d5e172ea84ca09b36f90d895
nofal <ahmednofal@aucegypt.edu>
2021-05-30 07:58:07 +0200

    verify single ported, merge test bench templates

--- Compiler/verification/gen_tb.py
+++ Compiler/verification/gen_tb.py
@@ -22,10 +22,42 @@ import sys
 import re
 import math
 import os
+def dual_ported_test(word_num, word_size, addr_width, model_filename):
+    tb = RAM_1RW1R_tb.RAM_instantiation_1RW1R.format(word_num=word_num,
+            word_size=word_size,
+            addr_width=addr_width,
+            filename=model_filename)
+    tb += RAM_1RW1R_tb.start_test_common
+    tb += RAM_1RW1R_tb.begin_dual_ported_test
+    tb += RAM_1RW1R_tb.dual_ported_custom_test
+    tb += RAM_1RW1R_tb.test_port_1RW1R
+    tb += RAM_1RW1R_tb.test_port_RW
+    tb += RAM_1RW1R_tb.end_test
+    tb += RAM_1RW1R_tb.tasks
+    tb += RAM_1RW1R_tb.dual_ported_tasks
+    tb += RAM_1RW1R_tb.endmodule
+
+    return tb
+
+def single_ported_test(word_num, word_size, addr_width, model_filename):
+    tb = RAM_1RW1R_tb.RAM_instantiation.format(word_num=word_num,
+            word_size=word_size,
+            addr_width=addr_width,
+            filename=model_filename)
+    tb += RAM_1RW1R_tb.start_test_common
+    tb += RAM_1RW1R_tb.begin_single_ported_test
+    tb += RAM_1RW1R_tb.single_ported_custom_test
+    tb += RAM_1RW1R_tb.test_port_RW
+    tb += RAM_1RW1R_tb.end_test
+    tb += RAM_1RW1R_tb.tasks
+    tb += RAM_1RW1R_tb.endmodule
+
+    return tb
 
 if __name__ == "__main__":
     PATTERN = sys.argv[1]
     word_num = int(re.search('RAM(\d+)x', PATTERN).group(1))
+    addr_width = int(math.log2(word_num))
     model_filename = os.path.realpath("../sky130A/BB/experimental/model.v")
     variant = RAM_1RW1R_tb
     if "1RW1R" in PATTERN:
@@ -33,18 +65,16 @@ if __name__ == "__main__":
         filename = 'tb_RAM{word_num}x{word_size}_1RW1R.v'.format(word_num=word_num,
         word_size=word_size)
         module = "RAM{word_num}_1RW1R".format(word_num=word_num)
+        tb = dual_ported_test(word_num, word_size, addr_width, model_filename)
     else:
         word_size = int(re.search(r"x(\d+)$", PATTERN).group(1))
         variant = RAM_tb
         filename = 'tb_RAM{word_num}x{word_size}.v'.format(word_num=word_num,
         word_size=word_size)
         module = "RAM{word_num}".format(word_num=word_num)
+        tb = single_ported_test(word_num, word_size, addr_width, model_filename)
 
 
-    addr_width = int(math.log2(word_num))
-    tb = variant.changeable_sub.format(word_num=word_num,
-            word_size=word_size, addr_width=addr_width, filename=model_filename)
-    tb += variant.constant_sub
     with open(filename, 'w+') as f:
         f.write(tb)
     print(module)
--- Compiler/verification/tb_template.py
+++ Compiler/verification/tb_template.py
@@ -1,4 +1,4 @@
-# Copyright ©2020-2021 The American University in Cairo and the Cloud V Project.
+ # Copyright ©2020-2021 The American University in Cairo and the Cloud V Project.
 #
 # This file is part of the DFFRAM Memory Compiler.
 # See https://github.com/Cloud-V/DFFRAM for further info.
@@ -15,7 +15,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-changeable_sub = """
+RAM_instantiation = """
 /*
     An auto generated testbench to verify RAM{word_num}x{word_size}
     Author: Mohamed Shalan (mshalan@aucegypt.edu)
@@ -45,10 +45,10 @@ module tb_RAM{word_num}x{word_size};
 
     reg                     CLK;
     reg  [(SIZE-1):0]       WE;
-    reg                     EN;
+    reg                     ENRW;
     reg  [(SIZE*8-1):0]     Di;
-    wire  [(SIZE*8-1):0]     Do;
-    reg  [A_W-1:0]          A, ADDR;
+    wire [(SIZE*8-1):0]     Do0;
+    reg  [A_W-1:0]          A0, ADDR;
     reg  [7:0]              Phase;
     reg  [7:0]      RANDOM_BYTE;
 
@@ -57,10 +57,10 @@ module tb_RAM{word_num}x{word_size};
     RAM{word_num} #(.USE_LATCH(`USE_LATCH), .WSIZE(SIZE)) SRAM (
         .CLK(CLK),
         .WE(WE),
-        .EN(EN),
+        .EN(ENRW),
         .Di(Di),
-        .Do(Do),
-        .A(A[A_W-1:$clog2(SIZE)])
+        .Do(Do0),
+        .A(A0[A_W-1:$clog2(SIZE)])
     );
 
     initial begin
@@ -69,38 +69,150 @@ module tb_RAM{word_num}x{word_size};
         @(done) $finish;
     end
 
-"""
-constant_sub="""
-    always #10 CLK = !CLK;
-
-    integer i;
-
      /* Memory golden Model */
     reg [(SIZE*8-1):0]      RAM[(M_SZ)-1 : 0];
-    reg [(SIZE*8-1):0]      RAM_DATA;
+    reg [(SIZE*8-1):0]      RAM_DATA_RW;
 
     genvar c;
     generate
     for (c=0; c < SIZE; c = c+1) begin: mem_golden_model
         always @(posedge CLK) begin
-            if(EN) begin
-                RAM_DATA <= RAM[A/SIZE];
-                if(WE[c]) RAM[A/SIZE][8*(c+1)-1:8*c] <= Di[8*(c+1)-1:8*c];
+            if(ENRW) begin
+                RAM_DATA_RW <= RAM[A0/SIZE];
+                if(WE[c]) RAM[A0/SIZE][8*(c+1)-1:8*c] <= Di[8*(c+1)-1:8*c];
             end
         end
     end
     endgenerate
+"""
+
+begin_single_ported_test = """
+    initial begin
+        CLK = 0;
+        WE = 0;
+        ENRW = 1;
+"""
+
+single_ported_custom_test = """
+        Phase = 0;
+        // Perform a single word write then read
+        mem_write_word({{SIZE{{8'h90}}}}, 4);
+        mem_read_word_0(4);
+"""
+
+RAM_instantiation_1RW1R = """
+/*
+    An auto generated testbench to verify RAM{word_num}x{word_size}
+    Authors:Mohamed Shalan (mshalan@aucegypt.edu)
+            Ahmed Nofal (nofal.o.ahmed@gmail.com)
+*/
+`define     VERBOSE_1
+`define     VERBOSE_2
+
+`define     UNIT_DELAY  #1
+
+`define     USE_LATCH   1
+
+`define     SIZE        {word_size}/8
+//`include "libs.ref/sky130_fd_sc_hd/verilog/primitives.v"
+//`include "libs.ref/sky130_fd_sc_hd/verilog/sky130_fd_sc_hd.v"
+
+`include "hd_primitives.v"
+`include "hd_functional.v"
+
+`include "{filename}"
+// `include "BB.v"
+
+module tb_RAM{word_num}x{word_size}_1RW1R;
+
+    localparam SIZE = `SIZE;
+    localparam A_W = {addr_width}+$clog2(SIZE);
+    localparam M_SZ = 2**A_W;
+
+    reg                   CLK;
+    reg  [(SIZE-1):0]       WE;
+    reg                  ENRW;
+    reg                  ENR;
+    reg  [(SIZE*8-1):0]   Di;
+    wire [(SIZE*8-1):0]   Do0;
+    wire [(SIZE*8-1):0]   Do1;
+    reg  [A_W-1:0]  A0, A1, ADDR;
+    reg  [7:0]      Phase;
+    reg  [7:0]      RANDOM_BYTE;
+    event           done;
+
+    RAM{word_num}_1RW1R #(.USE_LATCH(`USE_LATCH), .WSIZE(`SIZE)) SRAM (
+        .CLK(CLK),
+        .WE(WE),
+        .EN0(ENRW),
+        .EN1(ENR),
+        .Di(Di),
+        .Do0(Do0),
+        .Do1(Do1),
+        .A0(A0[A_W-1:$clog2(SIZE)]),
+        .A1(A1[A_W-1:$clog2(SIZE)])
+    );
+
+    initial begin
+        $dumpfile("tb_RAM{word_num}x{word_size}_1RW1R.vcd");
+        $dumpvars(0, tb_RAM{word_num}x{word_size}_1RW1R);
+        @(done) $finish;
+    end
+
+     /* Memory golden Model */
+    reg [(SIZE*8-1):0]      RAM[(M_SZ)-1 : 0];
+    reg [(SIZE*8-1):0]      RAM_DATA_RW;
+    reg [(SIZE*8-1):0]      RAM_DATA_R;
+
+    genvar c;
+    generate
+    for (c=0; c < SIZE; c = c+1) begin: mem_golden_model
+        always @(posedge CLK) begin
+            if(ENRW) begin
+                RAM_DATA_RW <= RAM[A0/SIZE];
+                if(WE[c]) RAM[A0/SIZE][8*(c+1)-1:8*c] <= Di[8*(c+1)-1:8*c];
+            end
+            if (ENR) begin
+                RAM_DATA_R <= RAM[A1/SIZE];
+            end
+        end
+    end
+    endgenerate
+"""
+
+begin_dual_ported_test = """
 
     initial begin
         CLK = 0;
         WE = 0;
-        EN = 1;
+        ENRW = 1;
+        ENR = 1;
 
+"""
+
+dual_ported_custom_test = """
         Phase = 0;
-        // Perform a single word write then read
-        mem_write_word({SIZE{8'h90}}, 4);
-        mem_read_word(4);
+        // Perform a 2 word write then read 2 words
+        mem_write_word({{SIZE{{8'h90}}}}, 4);
+        mem_write_word({{SIZE{{8'h33}}}}, 8);
+        mem_read_2words(4,8);
 
+"""
+
+start_test_common = """
+    always #10 CLK = !CLK;
+
+    integer i;
+"""
+
+test_port_1RW1R = """
+/***********************************************************
+
+        Write and read from different ports
+
+************************************************************/
+
+        // Fill the memory with a known pattern
         // Word Write then Read
         Phase = 1;
 `ifdef  VERBOSE_1
@@ -110,7 +222,7 @@ constant_sub="""
             ADDR = (($urandom%M_SZ)) & 'hFFFF_FFFC ;
             RANDOM_BYTE = $urandom;
             mem_write_word( {SIZE{RANDOM_BYTE}}, ADDR);
-            mem_read_word( ADDR );
+            mem_read_word_1( ADDR );
         end
 
         // HWord Write then Read
@@ -122,7 +234,7 @@ constant_sub="""
             ADDR = (($urandom%M_SZ)) & 'hFFFF_FFFE;
             RANDOM_BYTE = $urandom;
             mem_write_hword( {SIZE/2{RANDOM_BYTE}}, ADDR);
-            mem_read_word( ADDR & {{SIZE-1{8'hFF}}, 8'hFC} );
+            mem_read_word_1( ADDR & {{SIZE-1{8'hFF}}, 8'hFC} );
         end
 
         // Byte Write then Read
@@ -133,16 +245,63 @@ constant_sub="""
         for(i=0; i<M_SZ; i=i+1) begin
             ADDR = (($urandom%M_SZ));
             mem_write_byte($urandom%255, ADDR);
-            mem_read_word(ADDR & {{SIZE-1{8'hFF}}, 8'hFC} );
+            mem_read_word_1(ADDR & {{SIZE-1{8'hFF}}, 8'hFC} );
+        end
+
+"""
+
+test_port_RW = """
+/***********************************************************
+
+        Write and read from same port
+
+************************************************************/
+        Phase = 4;
+`ifdef  VERBOSE_1
+        $display("\\nFinished Phase 3, starting Phase 4");
+`endif
+        for(i=0; i<M_SZ; i=i+SIZE) begin
+            ADDR = (($urandom%M_SZ)) & 'hFFFF_FFFC ;
+            RANDOM_BYTE = $urandom;
+            mem_write_word( {SIZE{RANDOM_BYTE}}, ADDR);
+            mem_read_word_0( ADDR );
+        end
+
+        // HWord Write then Read
+        Phase = 5;
+`ifdef  VERBOSE_1
+        $display("\\nFinished Phase 4, starting Phase 5");
+`endif
+        for(i=0; i<M_SZ; i=i+SIZE/2) begin
+            ADDR = (($urandom%M_SZ)) & 'hFFFF_FFFE;
+            RANDOM_BYTE = $urandom;
+            mem_write_hword( {SIZE/2{RANDOM_BYTE}}, ADDR);
+            mem_read_word_0( ADDR & {{SIZE-1{8'hFF}}, 8'hFC} );
+        end
+
+        // Byte Write then Read
+        Phase = 6;
+`ifdef  VERBOSE_1
+        $display("\\nFinished Phase 5, starting Phase 6");
+`endif
+        for(i=0; i<M_SZ; i=i+1) begin
+            ADDR = (($urandom%M_SZ));
+            mem_write_byte($urandom%255, ADDR);
+            mem_read_word_0(ADDR & {{SIZE-1{8'hFF}}, 8'hFC} );
         end
         $display ("\\n>> Test Passed! <<\\n");
         -> done;
-    end
+"""
 
+end_test = """
+    end
+"""
+
+tasks = """
     task mem_write_byte(input [7:0] byte, input [A_W-1:0] addr);
     begin
         @(posedge CLK);
-        A = addr;//[A_WIDTH:2];
+        A0 = addr;//[A_WIDTH:2];
         WE = (1 << addr[$clog2(SIZE)-1:0]);
         Di = (byte << (addr[$clog2(SIZE)-1:0] * 8));
         @(posedge CLK);
@@ -156,7 +315,7 @@ constant_sub="""
     task mem_write_hword(input [SIZE*8-1:0] hword, input [A_W-1:0] addr);
     begin
         @(posedge CLK);
-        A = addr;//[A_WIDTH:$clog2(SIZE)];
+        A0 = addr;//[A_WIDTH:$clog2(SIZE)];
         WE = {{SIZE/2{addr[$clog2(SIZE)-1]}},{SIZE/2{~addr[$clog2(SIZE)-1]}}};
         Di = (hword << (addr[$clog2(SIZE)-1] * (SIZE/2)*8));
         @(posedge CLK);
@@ -170,7 +329,7 @@ constant_sub="""
     task mem_write_word(input [SIZE*8-1:0] word, input [A_W-1:0] addr);
     begin
         @(posedge CLK);
-        A = addr;
+        A0 = addr;
         WE = {SIZE{8'hFF}};
         Di = word;
         @(posedge CLK);
@@ -181,27 +340,73 @@ constant_sub="""
     end
     endtask
 
-    task mem_read_word(input [A_W-1:0] addr);
+    task mem_read_word_0(input [A_W-1:0] addr);
     begin
         @(posedge CLK);
-        A = addr;//[9:2];
+        A0 = addr;//[9:2];
         WE = {SIZE{8'h00}};
         @(posedge CLK);
         #5;
 `ifdef  VERBOSE_2
-        $display("READ WORD: 0x%X from %0D", Do, addr);
+        $display("READ WORD: 0x%X from %0D", Do0, addr);
 `endif
-        check();
+        check0();
     end
     endtask
 
-    task check; begin
-        if(RAM_DATA !== Do) begin
-            $display("\\n>>Test Failed! <<\\t(Pahse: %0d, Iteration: %0d", Phase, i);
-            $display("Address: 0x%X, READ: 0x%X - Should be: 0x%X", A, Do, RAM[A/SIZE]);
+    task check0; begin
+        if(RAM_DATA_RW !== Do0) begin
+            $display("\\n>>Test Failed! <<\\t(Phase: %0d, Iteration: %0d", Phase, i);
+            $display("Address: 0x%X, READ: 0x%X - Should be: 0x%X", A0, Do0, RAM[A0/SIZE]);
             $fatal(1);
         end
     end
     endtask
+
+"""
+dual_ported_tasks = """
+    task mem_read_2words(input [A_W-1:0] addr0,
+            input [A_W-1:0] addr1);
+    begin
+        @(posedge CLK);
+        A0= addr0;//[9:2];
+        A1= addr1;//[9:2];
+        WE = {SIZE{8'h00}};
+        @(posedge CLK);
+        #5;
+`ifdef  VERBOSE_2
+        $display("READ WORD0: 0x%X from %0D", Do0, addr0);
+        $display("READ WORD1: 0x%X from %0D", Do1, addr1);
+`endif
+        check0();
+        check1();
+    end
+    endtask
+
+    task mem_read_word_1(input [A_W-1:0] addr);
+    begin
+        @(posedge CLK);
+        A1 = addr;//[9:2];
+        WE = {SIZE{8'h00}};
+        @(posedge CLK);
+        #5;
+`ifdef  VERBOSE_2
+        $display("READ WORD: 0x%X from %0D", Do1, addr);
+`endif
+        check1();
+    end
+    endtask
+
+    task check1; begin
+        if(RAM_DATA_R !== Do1) begin
+            $display("\\n>>Test Failed! <<\\t(Phase: %0d, Iteration: %0d", Phase, i);
+            $display("Address: 0x%X, READ: 0x%X - Should be: 0x%X", A1, Do1, RAM[A1/SIZE]);
+            $fatal(1);
+        end
+    end
+    endtask
+"""
+
+endmodule = """
 endmodule
 """
