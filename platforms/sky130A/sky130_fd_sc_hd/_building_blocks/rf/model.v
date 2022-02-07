5cdd0e329ed1f5a795e38ea9c9619691f32881e3
mohamedleithy <leithy@aucegypt.edu>
2022-02-07 15:33:29 +0200

    + changed parameter 'RWIDTH' to WSIZE for the sake of consistency

--- platforms/sky130A/sky130_fd_sc_hd/_building_blocks/rf/model.v
+++ platforms/sky130A/sky130_fd_sc_hd/_building_blocks/rf/model.v
@@ -78,31 +78,31 @@ module DEC5x32 (
 	DEC2x4 D ( .A(A[4:3]), .SEL(EN), .EN(hi) );
 endmodule
 
-module RFWORD #(parameter RWIDTH=32) 
+module RFWORD #(parameter WSIZE=32) 
 (
     input   wire                CLK,
     input   wire                WE,
     input   wire                SEL1, 
     input   wire                SEL2, 
     input   wire                SELW,
-    output  wire [RWIDTH-1:0]   D1, D2,
-    input   wire [RWIDTH-1:0]   DW
+    output  wire [WSIZE-1:0]   D1, D2,
+    input   wire [WSIZE-1:0]   DW
 );
 
-    wire [RWIDTH-1:0]   q_wire;
+    wire [WSIZE-1:0]   q_wire;
     wire                we_wire;
-    wire [(RWIDTH/8)-1:0]          SEL1_B, SEL2_B;
-    wire [(RWIDTH/8)-1:0]          GCLK;
+    wire [(WSIZE/8)-1:0]          SEL1_B, SEL2_B;
+    wire [(WSIZE/8)-1:0]          GCLK;
 
-    sky130_fd_sc_hd__inv_4 INV1[(RWIDTH/8)-1:0] (.Y(SEL1_B), .A(SEL1));
-	sky130_fd_sc_hd__inv_4 INV2[(RWIDTH/8)-1:0] (.Y(SEL2_B), .A(SEL2));
+    sky130_fd_sc_hd__inv_4 INV1[(WSIZE/8)-1:0] (.Y(SEL1_B), .A(SEL1));
+	sky130_fd_sc_hd__inv_4 INV2[(WSIZE/8)-1:0] (.Y(SEL2_B), .A(SEL2));
 
     sky130_fd_sc_hd__and2_1 CGAND ( .A(SELW), .B(WE), .X(we_wire) );
-    sky130_fd_sc_hd__dlclkp_1 CG[(RWIDTH/8)-1:0] ( .CLK(CLK), .GCLK(GCLK), .GATE(we_wire) );
+    sky130_fd_sc_hd__dlclkp_1 CG[(WSIZE/8)-1:0] ( .CLK(CLK), .GCLK(GCLK), .GATE(we_wire) );
 
     generate 
         genvar i;
-        for(i=0; i<RWIDTH; i=i+1) begin : BIT
+        for(i=0; i<WSIZE; i=i+1) begin : BIT
             sky130_fd_sc_hd__dfxtp_1 FF ( .D(DW[i]), .Q(q_wire[i]), .CLK(GCLK[i/8]) );
             sky130_fd_sc_hd__ebufn_2 OBUF1 ( .A(q_wire[i]), .Z(D1[i]), .TE_B(SEL1_B[i/8]) );
 			sky130_fd_sc_hd__ebufn_2 OBUF2 ( .A(q_wire[i]), .Z(D2[i]), .TE_B(SEL2_B[i/8]) );
@@ -111,29 +111,29 @@ module RFWORD #(parameter RWIDTH=32)
     endgenerate 
 endmodule
 
-module RFWORD0 #(parameter RWIDTH=32)
+module RFWORD0 #(parameter WSIZE=32)
 (
     input   wire                CLK,
     input   wire                SEL1, 
     input   wire                SEL2, 
     input   wire                SELW,
-    output  wire [RWIDTH-1:0]   D1, D2
+    output  wire [WSIZE-1:0]   D1, D2
 );
 
-    wire [RWIDTH-1:0]           q_wire;
+    wire [WSIZE-1:0]           q_wire;
     wire                        we_wire;
-    wire [(RWIDTH/8)-1:0]                  SEL1_B, SEL2_B;
-    wire [(RWIDTH/8)-1:0]                  GCLK;
+    wire [(WSIZE/8)-1:0]                  SEL1_B, SEL2_B;
+    wire [(WSIZE/8)-1:0]                  GCLK;
 	wire [7:0]	                lo;
 
-    sky130_fd_sc_hd__inv_4 INV1[(RWIDTH/8)-1:0] (.Y(SEL1_B), .A(SEL1));
-	sky130_fd_sc_hd__inv_4 INV2[(RWIDTH/8)-1:0] (.Y(SEL2_B), .A(SEL2));
+    sky130_fd_sc_hd__inv_4 INV1[(WSIZE/8)-1:0] (.Y(SEL1_B), .A(SEL1));
+	sky130_fd_sc_hd__inv_4 INV2[(WSIZE/8)-1:0] (.Y(SEL2_B), .A(SEL2));
 
 	sky130_fd_sc_hd__conb_1 TIE [7:0] (.LO(lo), .HI());
 
     generate 
         genvar i;
-        for(i=0; i<RWIDTH; i=i+1) begin : BIT
+        for(i=0; i<WSIZE; i=i+1) begin : BIT
             sky130_fd_sc_hd__ebufn_2 OBUF1 ( .A(lo[i/8]), .Z(D1[i]), .TE_B(SEL1_B[i/8]) );
 			sky130_fd_sc_hd__ebufn_2 OBUF2 ( .A(lo[4+i/8]), .Z(D2[i]), .TE_B(SEL2_B[i/8]) );
         end
@@ -141,13 +141,13 @@ module RFWORD0 #(parameter RWIDTH=32)
 endmodule
 
 
-module DFFRF_2R1W #(parameter   RWIDTH=32,
+module DFFRF_2R1W #(parameter   WSIZE=32,
                                 RCOUNT=32,
                                 R0_ZERO=1 )
 (
 	input   wire    [4:0]                   RA, RB, RW,
-	input   wire    [RWIDTH-1:0]         	DW,
-	output  wire    [RWIDTH-1:0]        	DA, DB,
+	input   wire    [WSIZE-1:0]         	DW,
+	output  wire    [WSIZE-1:0]        	DA, DB,
 	input   wire                            CLK,
 	input   wire                            WE
 );
@@ -160,12 +160,12 @@ module DFFRF_2R1W #(parameter   RWIDTH=32,
 	generate
 		genvar e;
         if(R0_ZERO == 1)
-            RFWORD0 #(.RWIDTH(RWIDTH)) RFW0 ( .CLK(CLK), .SEL1(sel1[0]), .SEL2(sel2[0]), .SELW(selw[0]), .D1(DA), .D2(DB));	
+            RFWORD0 #(.WSIZE(WSIZE)) RFW0 ( .CLK(CLK), .SEL1(sel1[0]), .SEL2(sel2[0]), .SELW(selw[0]), .D1(DA), .D2(DB));	
         else
-            RFWORD #(.RWIDTH(RWIDTH)) RFW0 ( .CLK(CLK), .WE(WE), .SEL1(sel1[0]), .SEL2(sel2[0]), .SELW(selw[0]), .D1(DA), .D2(DB), .DW(DW) );
+            RFWORD #(.WSIZE(WSIZE)) RFW0 ( .CLK(CLK), .WE(WE), .SEL1(sel1[0]), .SEL2(sel2[0]), .SELW(selw[0]), .D1(DA), .D2(DB), .DW(DW) );
 
         for(e=1; e<RCOUNT; e=e+1) begin : REGF 
-			RFWORD #(.RWIDTH(RWIDTH)) RFW ( .CLK(CLK), .WE(WE), .SEL1(sel1[e]), .SEL2(sel2[e]), .SELW(selw[e]), .D1(DA), .D2(DB), .DW(DW) );	
+			RFWORD #(.WSIZE(WSIZE)) RFW ( .CLK(CLK), .WE(WE), .SEL1(sel1[e]), .SEL2(sel2[e]), .SELW(selw[e]), .D1(DA), .D2(DB), .DW(DW) );	
         end
 	endgenerate
 endmodule
