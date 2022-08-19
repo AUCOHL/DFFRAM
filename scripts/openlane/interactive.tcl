6fd250588037fbebc7605d60195aceedebb7d24c
Donn <me@donn.website>
2022-08-19 14:16:58 +0200

    Update OpenLane
    ~ openlane -> `57f3f21d6e1d0403a88d7a7973f7447597aae160`

--- scripts/openlane/interactive.tcl
+++ scripts/openlane/interactive.tcl
@@ -18,7 +18,8 @@ set_netlist $::env(INITIAL_NETLIST)
 set ::env(CURRENT_SDC) $::env(INITIAL_SDC)
 
 run_power_grid_generation
-run_routing
+global_routing
+detailed_routing
 run_parasitics_sta
 run_magic
 if {  [info exists ::env(ENABLE_KLAYOUT) ] } {
@@ -41,47 +42,6 @@ if {  [info exists ::env(ENABLE_CVC) ] } {
     if { ($::env(ENABLE_CVC) == 1)  } {
         run_lef_cvc
     }
-}
-
-proc save_final_views {args} {
-    set options {
-        {-save_path optional}
-    }
-    set flags {}
-    parse_key_args "save_final_views" args arg_values $options flags_map $flags
-
-    set arg_list [list]
-
-
-    # If they don't exist, save_views will simply not copy them
-    lappend arg_list -lef_path $::env(signoff_results)/$::env(DESIGN_NAME).lef
-    lappend arg_list -gds_path $::env(signoff_results)/$::env(DESIGN_NAME).gds
-    lappend arg_list -mag_path $::env(signoff_results)/$::env(DESIGN_NAME).mag
-    lappend arg_list -maglef_path $::env(signoff_results)/$::env(DESIGN_NAME).lef.mag
-    lappend arg_list -spice_path $::env(signoff_results)/$::env(DESIGN_NAME).spice
-    # Guaranteed to have default values
-    lappend arg_list -def_path $::env(CURRENT_DEF)
-    lappend arg_list -verilog_path $::env(CURRENT_NETLIST)
-
-    # Not guaranteed to have default values
-    if { [info exists ::env(CURRENT_SPEF)] } {
-        lappend arg_list -spef_path $::env(CURRENT_SPEF)
-    }
-    if { [info exists ::env(CURRENT_SDF)] } {
-        lappend arg_list -sdf_path $::env(CURRENT_SDF)
-    }
-    if { [info exists ::env(CURRENT_SDC)] } {
-        lappend arg_list -sdc_path $::env(CURRENT_SDC)
-    }
-
-    # Add the path if it exists...
-    if { [info exists arg_values(-save_path) ] } {
-        lappend arg_list -save_path $arg_values(-save_path)
-    }
-
-    # Aaand fire!
-    save_views {*}$arg_list
-
 }
 
 save_final_views -save_path $::env(PRODUCTS_PATH)
